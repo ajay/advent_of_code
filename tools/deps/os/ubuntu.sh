ac57376a46501ac4b1cda90c0fa5963788ddd821
Ajay Srivastava <ajaysriv3@gmail.com>
2023-12-01 11:54:58 -0800

    CI: update clang to v16

--- tools/deps/os/ubuntu.sh
+++ tools/deps/os/ubuntu.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-sudo apt install clang-15 clang-format-15 libboost-all-dev libfmt-dev googletest libgtest-dev python3 python3-pip
+sudo apt install libboost-all-dev libfmt-dev googletest libgtest-dev python3 python3-pip
+wget --directory-prefix /tmp https://apt.llvm.org/llvm.sh
+chmod +x /tmp/llvm.sh
+sudo /tmp/llvm.sh 16
+sudo apt install clang clang-format-16
