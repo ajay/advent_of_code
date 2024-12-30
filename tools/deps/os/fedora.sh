7af12778e45477230ef12c2a5fc911069f2847c7
Ajay Srivastava <ajaysriv3@gmail.com>
2024-12-30 12:47:17 -0800

    deps/os: reformat & add -y flag in deps install scripts

--- tools/deps/os/fedora.sh
+++ tools/deps/os/fedora.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 set -x -e
-sudo dnf update
-sudo dnf install clang clang-tools-extra boost boost-devel boost-regex fmt fmt-devel gtest gtest-devel python3 python3-pip
+sudo dnf -y update
+sudo dnf -y install 	\
+	boost 				\
+	boost-devel 		\
+	boost-regex 		\
+	clang 				\
+	clang-tools-extra 	\
+	fmt 				\
+	fmt-devel 			\
+	gtest 				\
+	gtest-devel 		\
+	python3 			\
+	python3-pip
--- tools/deps/os/ubuntu.sh
+++ tools/deps/os/ubuntu.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -x -e
-sudo apt install libboost-all-dev libfmt-dev googletest libgtest-dev python3 python3-pip
+sudo apt -y install 	\
+	googletest 			\
+	libboost-all-dev 	\
+	libfmt-dev 			\
+	libgtest-dev 		\
+	python3 			\
+	python3-pip
+
 wget --directory-prefix /tmp https://apt.llvm.org/llvm.sh
 chmod +x /tmp/llvm.sh
 sudo /tmp/llvm.sh 18
-sudo apt install clang clang-format-18
+sudo apt -y install 	\
+	clang 				\
+	clang-format-18
